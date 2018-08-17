4e661222472ab2b8b2eded9018aec73fccda6636
Lucasedu191 <lucasatletico@hotmail.com>
2018-08-17 17:02:06 -0300

    Criando programa, alterando listbox

---
+++ ProjetoModulo01/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ProjetoModulo01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnBotao1 = new System.Windows.Forms.Button();
+            this.Btnbotao2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LsblistaNomes = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnBotao1
+            // 
+            this.BtnBotao1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BtnBotao1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnBotao1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnBotao1.Location = new System.Drawing.Point(321, 61);
+            this.BtnBotao1.Name = "BtnBotao1";
+            this.BtnBotao1.Size = new System.Drawing.Size(75, 23);
+            this.BtnBotao1.TabIndex = 0;
+            this.BtnBotao1.Text = "Cadastrar";
+            this.BtnBotao1.UseVisualStyleBackColor = false;
+            this.BtnBotao1.Click += new System.EventHandler(this.BtnBotao1_Click);
+            // 
+            // Btnbotao2
+            // 
+            this.Btnbotao2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Btnbotao2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btnbotao2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btnbotao2.Location = new System.Drawing.Point(321, 121);
+            this.Btnbotao2.Name = "Btnbotao2";
+            this.Btnbotao2.Size = new System.Drawing.Size(75, 23);
+            this.Btnbotao2.TabIndex = 1;
+            this.Btnbotao2.Text = "Limpar";
+            this.Btnbotao2.UseVisualStyleBackColor = false;
+            this.Btnbotao2.Click += new System.EventHandler(this.Btnbotao2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(47, 61);
+            this.textBox1.MaxLength = 100;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(54, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nome:";
+            // 
+            // LsblistaNomes
+            // 
+            this.LsblistaNomes.BackColor = System.Drawing.Color.Aqua;
+            this.LsblistaNomes.FormattingEnabled = true;
+            this.LsblistaNomes.Location = new System.Drawing.Point(24, 108);
+            this.LsblistaNomes.Name = "LsblistaNomes";
+            this.LsblistaNomes.Size = new System.Drawing.Size(216, 160);
+            this.LsblistaNomes.TabIndex = 6;
+            this.LsblistaNomes.DoubleClick += new System.EventHandler(this.LsblistaNomes_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(38, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Lista de nomes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 271);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LsblistaNomes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Btnbotao2);
+            this.Controls.Add(this.BtnBotao1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "Frmmain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnBotao1;
+        private System.Windows.Forms.Button Btnbotao2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox LsblistaNomes;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProjetoModulo01/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoModulo01
+{
+    public partial class Form1 : Form
+    {
+        int iselecionado;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            iselecionado = -1;
+        } 
+        private void BtnBotao1_Click(object sender, EventArgs e)
+        {
+            if (iselecionado > -1)
+            {
+                LsblistaNomes.Items[iselecionado] = textBox1.Text;
+                Btnbotao2_Click(Btnbotao2, new EventArgs());
+                iselecionado = -1;
+                BtnBotao1.Text = "Cadastrar";
+                return;
+            }
+            LsblistaNomes.Items.Add(textBox1.Text);
+            //chama o evento click de limpar o text box
+            Btnbotao2_Click(Btnbotao2, new EventArgs());
+
+        }
+        private void ordenar ()
+        {
+           // list
+        }
+        // limpa a label e limpa o textbox
+        private void Btnbotao2_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = string.Empty;
+        }
+
+        // evento validating para aparecer a mensagem se for digitado apenas dois caracteres
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if(textBox1.Text.Length == null )
+            {
+                MessageBox.Show("Nome Invalido");
+            }
+        }
+          
+        // coloca a tecla enter como evento no text box e não precisa clicar no botão
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                BtnBotao1_Click(BtnBotao1, new EventArgs());
+            }
+        }
+
+        private void LsblistaNomes_DoubleClick(object sender, EventArgs e)
+        {
+            iselecionado = LsblistaNomes.SelectedIndex;
+            textBox1.Text = LsblistaNomes.Items[iselecionado].ToString();
+            BtnBotao1.Text = "Alterar";
+        }
+    }   
+}
